4f0852fe7fef006ec6814b78d1b3ec938eb275a6
VL Fanai <vfanai0@gmail.com>
2025-11-03 21:52:22 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-blue-100">
+      <Text className="text-5xl font-bold text-accent">Welcome</Text>
+    </View>
+  );
+}
